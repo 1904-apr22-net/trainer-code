945c146a1cde06015ff7c6fd290f031fd915352d
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-04-23 10:39:01 -0500

    add animals example

---
+++ 01-c-sharp/Animals/Animals.Library/Dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// from the terminal... (in Animals folder)
+//  ( dotnet new classlib --name Animals.Library )
+
+// we have a solution with two projects in it.
+namespace Animals.Library
+{
+    // ( mv Animals.Library/Class1.cs Animals.Library/Dog.cs )
+    public class Dog
+    {
+        // classes can contain methods.. they also contain data.
+        // normally we use "properties" to store data in classes.
+
+        public string Breed { get; set; }
+        public string Name { get; set; }
+
+        public void Bark()
+        {
+            Console.WriteLine("Bark");
+        }
+
+        // every Dog object will have his own breed, name, and ability to bark.
+    }
+}
---
+++ 01-c-sharp/Animals/Animals.UI/Program.cs
@@ -0,0 +1,35 @@
+﻿using Animals.Library;
+using System;
+
+// from the terminal...
+/*
+ * mkdir Animals
+ * cd Animals
+ * dotnet new console --name Animals.UI
+ * dotnet new sln
+ * dotnet sln add Animals.UI
+ */
+
+namespace Animals.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaring and instantiating a Dog
+            // to access the class in the other project, I need a project reference.
+            // (the lightbulb in VS can provide it)
+            // (in terminal, from Animals.UI folder,...)
+            // (dotnet add reference ../Animals.Library)
+            // (and, add "using Animals.Library;" at the top of this file)
+            Dog dog = new Dog();
+
+            // accessing properties and methods on objects.
+            dog.Name = "Bill";
+            dog.Breed = "Labrador";
+            dog.Bark();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
