cf442687aeec63c2fb589e8fc0658d8418a02095
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-04-25 13:39:44 -0500

    add delegates/events/lambda example.

---
+++ 01-c-sharp/Delegates/Delegates/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var movie = new Movie();
+            //movie.Name = "Infinity War";
+
+            var movie = new Movie { Name = "Infinity War" };
+
+            var moviePlayer = new MoviePlayer() { CurrentMovie = movie };
+
+            // subscribe to the event:
+            // first we need a function / method that should run
+            // when the event occurs.
+
+            // for the variable, the method itself is the value.
+            // the type is, the delegate type defined in MoviePlayer.
+            MoviePlayer.PlayFinishedHandler handler = EjectDisc;
+
+            MoviePlayer.PlayFinishedHandlerWithName handler2 = EjectDisc;
+
+            // subscribe to the event with +=
+            // (why +=? because we are adding one function to a list of
+            // other subscribing functions)
+            //moviePlayer.PlayFinished += handler2;
+
+            Action<string> handler3 = EjectDisc;
+
+            moviePlayer.PlayFinished += handler3;
+
+            Action<string> handler4 = name =>
+            {
+                Console.WriteLine("Handling with lambda expression");
+            };
+
+            moviePlayer.PlayFinished += handler4;
+
+            //moviePlayer.PlayFinished -= handler; // unsubscribe
+
+            moviePlayer.Play();
+        }
+
+        static void ExploringLambda()
+        {
+            Action<int, int> printFirstOne = (a, b) => { Console.WriteLine(a); };
+            printFirstOne(2, 4); // prints 2
+
+            // apart from Action, we also have Func type.
+            // Func is for all return types besides void.
+
+            // function that adds 1 to the input.
+            Func<int, int> addOne = x => x + 1;
+            Func<int, int> addOne2 = x => { return x + 1; }; // second way to write same thing
+            var two = addOne(1); // 2
+
+            // void return, three parameters
+            //Action<string, int, bool>
+
+            // bool return, string and int parameters.
+            //Func<string, int, bool>
+
+            // we use lambda expressions fairly often in C#,
+            // especially with LINQ.
+            var result = WrapWithPrints(addOne, 5); // print the inputs and the outputs
+        }
+
+        static TResult WrapWithPrints<TInput, TResult>(Func<TInput, TResult> func, TInput input)
+        {
+            Console.WriteLine($"input: {input}");
+            var result = func(input);
+            Console.WriteLine($"result: {result}");
+            return result;
+        }
+
+        static void EjectDisc()
+        {
+            Console.WriteLine("Ejecting the disc.");
+        }
+
+        static void EjectDisc(string name)
+        {
+            Console.WriteLine($"Ejecting disc {name}.");
+        }
+    }
+}
