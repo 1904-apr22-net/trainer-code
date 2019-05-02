fe4921c1a4ba40c46a49ec8179a70c916f6ce0df
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-05-02 11:36:52 -0500

    more ddl examples

--- 02-sql/ddl.sql
+++ 02-sql/ddl.sql
@@ -46,3 +46,43 @@ DROP TABLE Movie.Movie;
 --   prevents inserts or updates on the column, and gives automatic
 --   incrementing ID.
 --   (it is possible to switch on IDENTITY_INSERT)
+
+--DROP TABLE Movie.Movie;
+CREATE TABLE Movie.Movie (
+	MovieId INT NOT NULL IDENTITY,
+	Title NVARCHAR(200) NOT NULL,
+	ReleaseDate DATE NOT NULL,
+	DateModified DATETIME2 NOT NULL DEFAULT(GETDATE()),
+	Active BIT NOT NULL DEFAULT(1),
+	CONSTRAINT PK_MovieId PRIMARY KEY (MovieId),
+	CONSTRAINT U_Title_ReleaseDate UNIQUE (Title, ReleaseDate),
+	CONSTRAINT CK_DateNotTooEarly CHECK (ReleaseDate > '1900')
+);
+
+INSERT INTO Movie.Movie (Title, ReleaseDate) VALUES
+	('Avengers: Endgame', '2019');
+
+INSERT INTO Movie.Movie (MovieId, Title, ReleaseDate) VALUES
+	(8, 'asdf', '1019');
+
+CREATE TABLE Movie.Genre (
+	GenreId INT NOT NULL IDENTITY,
+	Name NVARCHAR(100) NOT NULL UNIQUE,
+	DateModified DATETIME2 NOT NULL DEFAULT(GETDATE()),
+	Active BIT NOT NULL DEFAULT(1)
+);
+
+ALTER TABLE Movie.Genre ADD
+	CONSTRAINT PK_GenreId PRIMARY KEY (GenreId);
+
+INSERT INTO Movie.Genre (Name) VALUES
+	('Action'); -- 1
+
+ALTER TABLE Movie.Movie ADD
+	GenreId INT NOT NULL DEFAULT(1),
+	CONSTRAINT FK_Movie_Genre FOREIGN KEY (GenreId)
+		REFERENCES Movie.Genre (GenreId)
+		ON DELETE CASCADE;
+
+ALTER TABLE Movie.Movie DROP
+	CONSTRAINT DF__Movie__GenreId__5535A963;
