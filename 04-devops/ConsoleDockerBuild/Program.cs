1e9ac43008385cc4093c08b1a44fd64ebe44ad02
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-05-16 16:48:51 -0500

    add some docker examples

---
+++ 04-devops/ConsoleDockerBuild/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleDockerBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
