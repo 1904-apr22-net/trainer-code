3807519b557035f396e51d5fe26ddb0d5d48a89d
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-05-29 10:35:02 -0500

    events and ajax demos

---
+++ 06-angular/javascript-ajax/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    let button = document.getElementById('jokeButton');
+    let header = document.getElementById('jokeText');
+
+    button.addEventListener('click', () => {
+        let xhr = new XMLHttpRequest();
+
+        xhr.addEventListener('readystatechange', () => {
+            // when readyState is 4, the response is ready
+            if (xhr.readyState === 4) {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    console.log(xhr.responseText);
+                    // deserialize JSON
+                    let jokeObj = JSON.parse(xhr.responseText);
+                    console.log(jokeObj);
+
+                    let joke = jokeObj.value.joke;
+
+                    // like innerHTML, but without parsing the HTML.
+                    header.textContent = joke;
+                }
+            }
+        });
+
+        xhr.open('GET', 'http://api.icndb.com/jokes/random?escape=javascript');
+
+        xhr.send();
+    });
+});
--- 06-angular/javascript-dom/main.js
+++ 06-angular/javascript-dom/main.js
@@ -14,3 +14,61 @@ window.onload = function () {
 };
 
 console.log('this runs first (body doesn\'t exist yet)');
+
+// event handler/listener same thing.
+
+// domobject.onload = function
+// domobject.addEventListener(function);
+
+// often we want code to run when the DOM is ready
+// for that, better than load event on window,
+// we can use DOMContentLoaded event on the document.
+
+document.addEventListener('DOMContentLoaded', () => {
+    let button1 = document.getElementById('button1');
+
+    button1.addEventListener('click', () => {
+        let newPara = document.createElement('p');
+        newPara.innerHTML = 'new paragraph';
+        document.body.appendChild(newPara);
+    });
+
+    let link = document.querySelector('a');
+    link.addEventListener('click', event => {
+        // disable browser's default
+        // behavior on this event on this element
+        event.preventDefault();
+
+        // this is how to navigate to a new page
+        window.location.href = 'https://microsoft.com';
+
+        button1.innerHTML = 'clicked';
+    });
+
+    let table = document.querySelector('table');
+
+    let firstTd = table.tBodies[0].rows[0].cells[0];
+
+    firstTd.addEventListener('dblclick', printEventDetails);
+
+    table.addEventListener('dblclick', printEventDetails);
+
+    window.addEventListener('dblclick', printEventDetails, true);
+});
+
+function printEventDetails(event) {
+    // prevents the event from continuing
+    // to bubble/capture to another element.
+    event.stopPropagation();
+
+    // even stops other event handlers
+    // on this same element.
+    event.stopImmediatePropagation();
+
+
+    console.log(`event.type: ${event.type}`);
+    console.log(`event.target: ${event.target}`);
+    console.log(`event.currentTarget: ${event.currentTarget}`);
+    console.log(`this: ${this}`);
+    console.log('----------------');
+}
