a36f274f8289f9b070fe61d93de4468730c33aa3
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-05-29 12:00:43 -0500

    typescript with npm

---
+++ 06-angular/typescript-demo/src/index.ts
@@ -0,0 +1,11 @@
+class Main {
+    constructor() { }
+
+    static Main() {
+        document.addEventListener('DOMContentLoaded', () => {
+            console.log('log from typescript');
+        });
+    }
+}
+
+Main.Main();
