da547a739ab12ff1ec13f1c35de107f9f147871d
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-05-30 10:37:36 -0500

    add deck of cards example to ng demo

--- 06-angular/angular-demo/src/app/app.component.ts
+++ 06-angular/angular-demo/src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template: `
+  // <div>
+
+  // </div>
+  // `,
   templateUrl: './app.component.html',
+  // styles: [
+  //   ''
+  // ]
   styleUrls: ['./app.component.css']
+  // inputs, outputs
 })
 export class AppComponent {
   title = 'angular-demo';
+
+
 }
