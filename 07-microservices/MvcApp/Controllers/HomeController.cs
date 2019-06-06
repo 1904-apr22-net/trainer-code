6a29bc5cfb2715add4fd2ffd2df8136fc189d1e9
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-06-06 15:14:29 -0500

    add api service and request from mvc.

--- 07-microservices/MvcApp/Controllers/HomeController.cs
+++ 07-microservices/MvcApp/Controllers/HomeController.cs
@@ -2,16 +2,34 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using MvcApp.Models;
 
 namespace MvcApp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _httpClient;
+        private readonly string _apiUrl;
+
+        public HomeController(HttpClient httpClient, IConfiguration configuration)
         {
+            _httpClient = httpClient;
+            _apiUrl = configuration["ApiUrl"];
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync(_apiUrl);
+            var data = await response.Content.ReadAsAsync<string[]>();
+
+            var dataString = string.Join(", ", data);
+
+            ViewData["data"] = dataString;
+
             return View();
         }
 
